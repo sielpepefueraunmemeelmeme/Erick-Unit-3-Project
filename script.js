c6e12244cfb81b164ad3b095819b34cd6d7ab990
sielpepefueraunmemeelmeme <125302339+sielpepefueraunmemeelmeme@users.noreply.github.com>
2023-02-13 12:22:38 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+let confrontButton=document.querySelector(".option-one");	
+let buildButton=document.querySelector(".option-two");
+//two different first option screen
+let confrontDiv=document.querySelector(".option-one-screen");
+let buildDiv=document.querySelector(".option-two-screen");
+//second button options
+let ohNoButton=document.querySelector(".ohno");
+let runButton=document.querySelector(".run");
+//two different end option screens
+let KidnappDiv=document.querySelector(".option-one-end");
+let savedDiv=document.querySelector(".option-two-end");
+//body 
+let body= document.querySelector("body");
